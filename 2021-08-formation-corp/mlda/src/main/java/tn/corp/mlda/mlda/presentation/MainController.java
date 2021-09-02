07fd864abe6b565041b7069d8a032ae196d9a620
thevpc <taha.bensalah@gmail.com>
2021-09-02 15:11:03 +0100

    jour 4

--- 2021-08-formation-corp/mlda/src/main/java/tn/corp/mlda/mlda/presentation/MainController.java
+++ 2021-08-formation-corp/mlda/src/main/java/tn/corp/mlda/mlda/presentation/MainController.java
@@ -3,13 +3,23 @@ package tn.corp.mlda.mlda.presentation;
 import java.util.Iterator;
 import java.util.List;
 
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ListChangeListener.Change;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -20,21 +30,30 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polyline;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
 import tn.corp.mlda.mlda.model.Album;
 import tn.corp.mlda.mlda.service.AlbumService;
 
 public class MainController {
 	@FXML
-	private ChoiceBox<String> anneeFilter;
+	private ChoiceBox<String> yearFilter;
 
 	@FXML
 	private ChoiceBox<String> albumFilter;
 
 	@FXML
-	private ChoiceBox<String> chanteurFilter;
+	private ChoiceBox<String> singerFilter;
 
 	@FXML
 	private Button myButton;
@@ -47,39 +66,190 @@ public class MainController {
     private VBox board;
     
 	@FXML
-	void clickMe(ActionEvent event) {
-
-	}
-
-	@FXML
 	private ListView<String> categoryList;
 
+	
+    @FXML
+    private BarChart<?, ?> barChart;
+    
 	@FXML
 	public void initialize() {
+		
+		service.loadData();
+		
 		categoryList.getItems().add("<Tous>");
-		categoryList.getItems().add("Pop");
-		categoryList.getItems().add("Oriential");
-		categoryList.getItems().add("Funk");
-		categoryList.getItems().add("Blues");
-		categoryList.getItems().add("Classical");
-		categoryList.getItems().add("Rock");
-		categoryList.getItems().add("Rock&Roll");
-		categoryList.getItems().add("Hard Rock");
-		categoryList.getSelectionModel().getSelectedItems().addListener(this::onChangeListItem);
+		categoryList.getItems().addAll(service.findCategoryNames());
+		categoryList.getSelectionModel().getSelectedItems().addListener(this::onChangeCategory);
 
-		anneeFilter.getItems().add("<Tous>");
-		anneeFilter.getItems().add("2021");
-		anneeFilter.getItems().add("2020");
-		anneeFilter.getItems().add("2019");
-		anneeFilter.getSelectionModel().selectedItemProperty().addListener(this::onChangeAnnee);
+		yearFilter.getItems().add("<Tous>");
+		yearFilter.getItems().addAll(service.findYearsNames());
+		yearFilter.getSelectionModel().selectedItemProperty().addListener(this::onChangeYear);
 
+		singerFilter.getItems().add("<Tous>");
+		singerFilter.getItems().addAll(service.findSingerNames());
+		singerFilter.getSelectionModel().selectedItemProperty().addListener(this::onChangeSinger);
+
+		reAppliquerFitre();
+		
+		prepareBoard();
+		prepareBarChart();
+	}
+
+	public void prepareBoard() {
+		Group p=new Group();
+	    Shape M=new Polyline(
+					0, 24, 
+					0, 0,
+					12, 12,
+					24, 0,
+					24, 24
+					);
+		int x= 30;
+	    Shape L=new Polyline(
+				x+0, 0, 
+				x+0, 24,
+				x+24, 24
+				);
+		x= 60;
+		Shape D=new Arc(
+				x+0, 12, 
+				24, 12,
+				-90, 180
+				);
+		x= 90;
+		Shape A=new Polyline(
+				x+0, 24, 
+				x+0, 0,
+				x+24, 0,
+				x+24, 12,
+				x+0, 12,
+				x+24, 12,
+				x+24, 24
+				);
+		
+		//
+
+		M.setStrokeWidth(3.0);
+	    M.setStroke(Color.YELLOWGREEN);
+
+	    L.setStrokeWidth(3.0);
+	    L.setStroke(Color.DARKBLUE);
+
+		D.setStrokeWidth(3.0);
+	    D.setStroke(Color.YELLOWGREEN);
+	    D.setFill(Color.GREENYELLOW);
+		
+	    A.setStrokeWidth(3.0);
+	    A.setStroke(Color.DARKBLUE);
+
+	    p.getChildren().add(M);
+		p.getChildren().add(L);
+		p.getChildren().add(D);
+		p.getChildren().add(A);
+//		board.getChildren().add(
+//				new Circle(10, 10, 10)
+//		);
+		board.getChildren().add(p);
+		Label descr=new Label("Media Library Desktop Application");
+		descr.setFont(new Font(8));
+		board.getChildren().add(descr);
+		
+		TranslateTransition tr=new TranslateTransition();
+		tr.setDuration(Duration.seconds(3));
+		tr.setAutoReverse(true);
+		tr.setCycleCount(Animation.INDEFINITE);
+		tr.setToY(30);
+		tr.setNode(board);
+		tr.play();
+	}
+	
+//	public Node createVignetteByFXML(Album a) {
+//		try {
+//			Node vignette = App.loadFXML("vignette");
+//			ImageView imageView = (ImageView) vignette.lookup("image");
+//			Label title = (Label) vignette.lookup("title");
+//			Label description = (Label) vignette.lookup("description");
+//			title.setText(a.getTitle());
+//			description.setText(a.getDescription());
+//			imageView.setImage(new Image(a.getImage()));
+//			return vignette;
+//		} catch (Exception ex) {
+//			throw new IllegalArgumentException(ex);
+//		}
+//	}
+
+	public Node createVignetteByCode(Album a) {
+		BorderPane control = new BorderPane();
+		control.setPadding(new Insets(10));
+		ImageView image = new ImageView();
+		control.setCenter(image);
+		VBox vb = new VBox();
+		VBox.setVgrow(vb, Priority.ALWAYS);
+		vb.setFillWidth(true);
+		control.setBottom(vb);
+		Label title = new Label();
+		vb.getChildren().add(title);
+		Label description = new Label();
+		vb.getChildren().add(description);
+
+		image.setImage(new Image(getClass().getResourceAsStream(a.getImage())));
+		image.setFitWidth(64);
+		image.setFitHeight(64);
+		title.setText(a.getTitle());
+		title.setAlignment(Pos.CENTER);
+		description.setText(a.getDescription());
+		description.setAlignment(Pos.CENTER);
+		
+		
+		final FadeTransition fadeIn = new FadeTransition(Duration.millis(100));
+        fadeIn.setNode(control);
+        fadeIn.setToValue(1);
+        control.setOnMouseEntered(e -> fadeIn.playFromStart());
+
+        final FadeTransition fadeOut = new FadeTransition(Duration.millis(100));
+        fadeOut.setNode(control);
+        fadeOut.setToValue(0.5);
+        control.setOnMouseExited(e -> fadeOut.playFromStart());
+
+        control.setOpacity(0.8);
+        
+        
+        RotateTransition rotation = new RotateTransition(Duration.seconds(0.5), control);
+        rotation.setCycleCount(Animation.INDEFINITE);
+        rotation.setByAngle(360);
+        control.setOnMouseEntered(e -> rotation.playFromStart());
+        control.setOnMouseExited(e -> {
+        	control.setRotate(0);
+        	rotation.pause();
+        });
+        
+		return control;
+	}
+
+	void onChangeYear(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		reAppliquerFitre();
+	}
+	
+	void onChangeSinger(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		reAppliquerFitre();
+	}
+	
+	public void onChangeCategory(Change<? extends String> ch) {
+		reAppliquerFitre();
+	}
+
+	void reAppliquerFitre() {
+		musicBox.getChildren().clear();
 		String category = categoryList.getSelectionModel().getSelectedItem();
 		if ("<Tous>".equals(category)) {
 			category = null;
 		}
-		String year = anneeFilter.getSelectionModel().getSelectedItem();
+		String singer = singerFilter.getSelectionModel().getSelectedItem();
+		if ("<Tous>".equals(singer)) {
+			singer = null;
+		}
+		String year = yearFilter.getSelectionModel().getSelectedItem();
 		Integer yearInt = (year == null || year.equals("<Tous>")) ? null : Integer.parseInt(year);
-		String singer = null;
 		List<Album> theAlbums = service.findAlbums(yearInt, category, singer);
 
 		int columns = musicBox.getColumnCount();
@@ -90,72 +260,15 @@ public class MainController {
 					int p = i * columns + j;
 					if(p<theAlbums.size()) {
 						Album a = theAlbums.get(p);
-						Node vignette1=createVignetteByFXML(a);
+//						Node vignette1=createVignetteByFXML(a);
 						Node vignette2=createVignetteByCode(a);
-						musicBox.add(vignette1, i, j);
+						musicBox.add(vignette2, i, j);
 					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
 		}
-		
-		prepareBoard();
-	}
-
-	public void prepareBoard() {
-		board.getChildren().add(
-				new Line(0, 0, 20, 20)
-		);
-		board.getChildren().add(
-				new Circle(10, 10, 10)
-		);
-	}
-	
-	public Node createVignetteByFXML(Album a) {
-		try {
-			Node vignette = App.loadFXML("vignette");
-			ImageView imageView = (ImageView) vignette.lookup("image");
-			Label title = (Label) vignette.lookup("title");
-			Label description = (Label) vignette.lookup("description");
-			title.setText(a.getTitle());
-			description.setText(a.getDescription());
-			imageView.setImage(new Image(a.getImage()));
-			return vignette;
-		} catch (Exception ex) {
-			throw new IllegalArgumentException(ex);
-		}
-	}
-
-	public Node createVignetteByCode(Album a) {
-		BorderPane bp = new BorderPane();
-		ImageView iv = new ImageView();
-		bp.setCenter(iv);
-		VBox vb = new VBox();
-		bp.setBottom(vb);
-		Label title = new Label();
-		vb.getChildren().add(title);
-		Label description = new Label();
-		vb.getChildren().add(description);
-
-		iv.setImage(new Image(a.getImage()));
-		title.setText(a.getTitle());
-		description.setText(a.getDescription());
-		return bp;
-	}
-
-	void onChangeAnnee(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-		reAppliquerFitre();
-	}
-
-	public void onChangeListItem(Change<? extends String> ch) {
-		String selectedItem = categoryList.getSelectionModel().getSelectedItem();
-		// faire ce qu'il faut quand on selectionne
-		reAppliquerFitre();
-	}
-
-	void reAppliquerFitre() {
-
 	}
 
 	@FXML
@@ -164,6 +277,31 @@ public class MainController {
 		alert.setContentText("Malla Ra7a");
 		alert.show();
 		// System.exit(0);
+	}
+	
+	public void prepareBarChart() {
+		barChart.getXAxis().setLabel("Albums");
+		barChart.getYAxis().setLabel("Visits");
+		barChart.getData().clear();
+		XYChart.Series dataSeries1 = new XYChart.Series();
+		dataSeries1.setName("2014");
+
+		String category = categoryList.getSelectionModel().getSelectedItem();
+		if ("<Tous>".equals(category)) {
+			category = null;
+		}
+		String singer = singerFilter.getSelectionModel().getSelectedItem();
+		if ("<Tous>".equals(singer)) {
+			singer = null;
+		}
+		String year = yearFilter.getSelectionModel().getSelectedItem();
+		Integer yearInt = (year == null || year.equals("<Tous>")) ? null : Integer.parseInt(year);
+		List<Album> theAlbums = service.findAlbums(yearInt, category, singer);
+
+		for (Album album : theAlbums) {
+			dataSeries1.getData().add(new XYChart.Data(album.getTitle(), (int)(Math.random()*200)));
+		}
+		barChart.getData().add(dataSeries1);
 	}
 
 }
